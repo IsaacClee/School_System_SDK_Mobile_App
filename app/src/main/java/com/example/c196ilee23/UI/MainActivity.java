723cd280fa77f9d2df14cfd8b6d85813972e1e61
IsaacClee <51291012+IsaacClee@users.noreply.github.com>
2022-08-07 16:41:57 -0500

    Adjusted main activity components

--- app/src/main/java/com/example/c196ilee23/UI/MainActivity.java
+++ app/src/main/java/com/example/c196ilee23/UI/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.c196ilee23.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.c196ilee23.R;
 
@@ -13,4 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void EnterHere(View view){
+        Intent intent= new Intent(MainActivity.this, ProductList.class );
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/c196ilee23/UI/ProductList.java
+++ app/src/main/java/com/example/c196ilee23/UI/ProductList.java
@@ -3,6 +3,8 @@ package com.example.c196ilee23.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.c196ilee23.R;
 
@@ -12,5 +14,26 @@ public class ProductList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_productlist, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+
 }
