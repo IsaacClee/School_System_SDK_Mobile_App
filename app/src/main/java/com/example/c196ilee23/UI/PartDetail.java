5250878cd3bb05b1d3899de23ba1e2f7cccfd008
IsaacClee <51291012+IsaacClee@users.noreply.github.com>
2022-08-21 17:07:32 -0500

    Adding in Parts Detail layout

--- app/src/main/java/com/example/c196ilee23/UI/PartDetail.java
+++ app/src/main/java/com/example/c196ilee23/UI/PartDetail.java
@@ -2,19 +2,53 @@ package com.example.c196ilee23.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.c196ilee23.R;
 
-public class PartDetail extends AppCompatActivity {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
+public class PartDetail extends AppCompatActivity {
+    EditText editDate;
+    DatePickerDialog.OnDateSetListener startDate;
+    final Calendar myCalenderStart = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part_detail);
+        editDate = findViewById(R.id.editDate);
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        editDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Date date;
+                String info = editDate.getText().toString();
+                if(info.equals("")) info = "08/21/2022";
+                try {
+                    myCalenderStart.setTime(sdf.parse(info));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                new DatePickerDialog(
+                        PartDetail.this,
+                        startDate,
+                        myCalenderStart.get(Calendar.YEAR),
+                        myCalenderStart.get(Calendar.MONTH),
+                        myCalenderStart.get(Calendar.DAY_OF_MONTH))
+                        .show();
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
