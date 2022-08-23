07f4ac40ff31bdd6b9d462b0c1798a68a73555ad
IsaacClee <51291012+IsaacClee@users.noreply.github.com>
2022-08-23 16:05:02 -0500

    App now generates term details

---
+++ app/src/main/java/com/example/c196ilee23/UI/AssessmentList.java
@@ -0,0 +1,16 @@
+package com.example.c196ilee23.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.c196ilee23.R;
+
+public class AssessmentList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessement_list);
+    }
+}
--- app/src/main/java/com/example/c196ilee23/UI/MainActivity.java
+++ app/src/main/java/com/example/c196ilee23/UI/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.c196ilee23.DataBase.Repository;
+import com.example.c196ilee23.Entity.Course;
 import com.example.c196ilee23.Entity.Part;
 import com.example.c196ilee23.Entity.Product;
 import com.example.c196ilee23.Entity.Term;
@@ -27,9 +28,47 @@ public class MainActivity extends AppCompatActivity {
         Term term = new Term(1, "Term 1", 1661271686, 1661271686);
         Term term2 = new Term(2, "Term 2", 1661271686, 1661271686);
         Term term3 = new Term(3, "Term 3", 1661271686, 1661271686);
+        Term term4 = new Term(4, "Term 4", 1661271686, 1661271686);
         repo.insert(term);
         repo.insert(term2);
         repo.insert(term3);
+        repo.insert(term4);
+        Course course = new Course(
+                1,
+                "English",
+                1661271686,
+                1661271686,
+                "Enrolled",
+                "Dr. Martin Jones",
+                "982-765-4839",
+                "skeoxdr@gmail.com",
+                "N/A",
+                "Term 1" );
+        Course course2 = new Course(
+                2,
+                "History",
+                1661271686,
+                1661271686,
+                "Enrolled",
+                "Dr. Emma Johnson",
+                "982-765-4839",
+                "johnsomE@gmail.com",
+                "N/A",
+                "Term 1" );
+        Course course3 = new Course(
+                3,
+                "Math",
+                1661271686,
+                1661271686,
+                "Plan to Take",
+                "Dr. Sam Sampson",
+                "982-765-4839",
+                "lasampsondr@gmail.com",
+                "N/A",
+                "Term 2" );
+        repo.insert(course);
+        repo.insert(course2);
+        repo.insert(course3);
         Product product = new Product(2, "Bike", 200.00);
         repo.insert(product);
         Part part = new Part(2,"Brake", 35.00, 2);
